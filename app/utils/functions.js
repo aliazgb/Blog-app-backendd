429644a5dc5872b2bd4b15c614cc73746c10b7cc
aliazgb <ali.azgb10@gmail.com>
2025-09-05 16:45:41 +0200

    test

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -43,28 +43,28 @@ function generateToken(user, expiresIn, secret) {
 }
 
 async function setAccessToken(res, user) {
-  const cookieOptions = {
-    maxAge: 1000 * 60 * 60 * 24 * 1, // 1 روز
-    httpOnly: true,
-    signed: true,
-    sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None", // cross-site روی production
-    secure: process.env.NODE_ENV === "development" ? false : true,
-    path: "/",
-  };
+const cookieOptions = {
+  maxAge: 1000 * 60 * 60 * 24 * 365, 
+  httpOnly: true,
+  signed: true,
+  sameSite: "None",
+  secure: true,
+};
+
 
   const token = await generateToken(user, "1d", process.env.ACCESS_TOKEN_SECRET_KEY);
   res.cookie("accessToken", token, cookieOptions);
 }
 
 async function setRefreshToken(res, user) {
-  const cookieOptions = {
-    maxAge: 1000 * 60 * 60 * 24 * 365, // 1 سال
-    httpOnly: true,
-    signed: true,
-    sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
-    secure: process.env.NODE_ENV === "development" ? false : true,
-    path: "/",
-  };
+const cookieOptions = {
+  maxAge: 1000 * 60 * 60 * 24 * 365, 
+  httpOnly: true,
+  signed: true,
+  sameSite: "None",
+  secure: true,
+};
+
 
   const token = await generateToken(user, "1y", process.env.REFRESH_TOKEN_SECRET_KEY);
   res.cookie("refreshToken", token, cookieOptions);
